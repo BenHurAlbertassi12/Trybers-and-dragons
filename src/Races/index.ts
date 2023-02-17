1a2937bc6e1064e34bf21019bae928fe45bf59ba
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-02-17 11:22:09 -0300

    Req 1

---
+++ src/Races/index.ts
@@ -0,0 +1,9 @@
+import Race from './Race';
+
+// import Dwarf from './Dwarf';
+import Elf from './Elf'; 
+// import Halfling from './Halfling';
+// import Orc from './Orc';
+
+export { Elf };
+export default Race;
