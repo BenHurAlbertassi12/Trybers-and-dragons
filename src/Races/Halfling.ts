4cd4b063000c8ce6e3bd2aeac866b85027f352a2
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-02-17 11:56:26 -0300

    Halfling.ts

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -1,4 +1,26 @@
 import Race from './Race';
 
-class Halfling extends Race {}
+class Halfling extends Race {
+  private _maxLifePoints: number;
+  static count = 0;
+
+  constructor(name: string, dexterity: number, maxLifePoints = 60) {
+    super(name, dexterity);
+    this._maxLifePoints = maxLifePoints;
+    Halfling.count += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  set maxLifePoints(value: number) {
+    this._maxLifePoints = value;
+  }
+
+  static createdRacesInstances(): number {
+    return Halfling.count;
+  }
+}
+
 export default Halfling;
