db45629844d2b68ac524489c8b8933715e828ee7
BenHur Albertassi <benhuralber@yahoo.com.br>
2023-02-17 11:57:31 -0300

    Orc

--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -1,4 +1,26 @@
 import Race from './Race';
 
-class Orc extends Race {}
+class Orc extends Race {
+  private _maxLifePoints: number;
+  static count = 0;
+
+  constructor(name: string, dexterity: number, maxLifePoints = 74) {
+    super(name, dexterity);
+    this._maxLifePoints = maxLifePoints;
+    Orc.count += 1;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  set maxLifePoints(value: number) {
+    this._maxLifePoints = value;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc.count;
+  }
+}
+
 export default Orc;
--- src/Races/index.ts
+++ src/Races/index.ts
@@ -3,7 +3,7 @@ import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf'; 
 import Halfling from './Halfling';
-// import Orc from './Orc';
+import Orc from './Orc';
 
-export { Elf, Dwarf, Halfling };
+export { Elf, Dwarf, Halfling, Orc };
 export default Race;
